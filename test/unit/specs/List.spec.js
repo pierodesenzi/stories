0a2951f4cfc5fd4164107f9eac9c8668a1f1e2d5
piero <piero.desenzi@gmail.com>
2020-09-25 20:47:31 -0300

    Initial test module

---
+++ test/unit/specs/List.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils'
+import List from '@/components/List.vue'
+
+describe('List.vue', () => {
+    const wrapper = shallowMount(List)
+
+    it ('Exists', () => {
+        expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+})
