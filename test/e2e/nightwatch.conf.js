84375242f684167f17553ed0c6ff4d9a5d043a6c
Luis <lhzimmermann@gmail.com>
2020-11-14 10:37:27 -0300

    Change Chromedriver PATH

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -13,7 +13,7 @@ module.exports = {
     host: '127.0.0.1',
     port: 4444,
     cli_args: {
-      'webdriver.chrome.driver': require('chromedriver').path
+      'webdriver.chrome.driver': "/usr/local/bin/chromedriver" //require('chromedriver').path
     }
   },
 
