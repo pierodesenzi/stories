a833d2ab15a905d617bb332bfe59b6dd04507a3a
Luis <lhzimmermann@gmail.com>
2020-11-20 11:29:08 -0300

    Removed some duplicate unit tests
    Removed some duplicated unit tests because i believe they're supposed to be e2e tests

--- test/unit/register.test.js
+++ test/unit/register.test.js
@@ -17,45 +17,6 @@ describe('Register', () => {
       expect(wrapper.get('form')).toBeTruthy
   })
 
-  it('validates if fields are empty', ()=>{
-
-    const button = wrapper.find('#btnSubmit')
-    button.trigger('click').then(resp =>{
-        expect(wrapper.html()).toContain('This field is required')
-        done();
-    })
-    .catch(err =>{
-      throw err;
-    })
-  })
-
-  it('insert if data is valid, then redirect', ()=>{
-    wrapper.setData({
-        email: 'ayrofllmao@gmail.com',
-        password: '123456',
-        username:'risos',
-        name:{
-            first: 'risonho',
-            last:'da silva',
-        },
-        biography: 'hhahahahahah',
-    })
-    .then(ret =>{
-        const button = wrapper.find('#btnSubmit')
-        button.trigger('click').then(resp =>{
-            expect(wrapper.vm._isDestroyed).toBeTruthy;
-            done()
-        })
-        .catch(err =>{
-          throw err;
-        })
-    })
-    .catch(err =>{
-      throw err;
-    })
-
-    
-  })
 
 //   it('has not a register method', () => {
 //     expect(typeof List.created).toBe('function')
