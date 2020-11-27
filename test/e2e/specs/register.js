e67213421c2e26475eb3a282d3fa7faf8ecd70a5
Luis <lhzimmermann@gmail.com>
2020-11-27 10:48:16 -0300

    More register tests

--- test/e2e/specs/register.js
+++ test/e2e/specs/register.js
@@ -4,17 +4,13 @@ module.exports = {
     password:  '123456',
 
      
-    'validating empty fields': function (browser) {
-      // automatically uses dev Server port from /config.index.js
-      // default: http://localhost:8080
-      // see nightwatch.conf.js
+    'validates empty fields': function (browser) {
       const devServer = browser.globals.devServerURL
   
       browser
         .url(devServer)
-        .waitForElementVisible('#app', 5000)
         .useXpath()
-        .assert.visible("//a[text()='Register']")
+        .waitForElementVisible("//a[text()='Register']", 5000)
         .click("//a[text()='Register']")
         .useCss()
         .assert.visible("button[type=submit]")
@@ -23,30 +19,62 @@ module.exports = {
         .assert.visible("//span[text()='This field is required']")
         .end()
     },
-    'inserts a user': function (browser) {
-        // automatically uses dev Server port from /config.index.js
-        // default: http://localhost:8080
-        // see nightwatch.conf.js
-        const devServer = browser.globals.devServerURL
-    
-        browser
-          .url(devServer)
-          .useXpath()
-          .waitForElementVisible("//a[text()='Register']", 5000)
-          .click("//a[text()='Register']")
-          .useCss()
 
-          .assert.visible("button[type=submit]")
-          browser.setValue('input[id=email]', 'ayrofllmao@gmail.com')
-          browser.setValue('input[id=password]', '123456')
-          browser.setValue('input[id=username]', 'risos')
-          browser.setValue('input[id=firstName]', 'risonho')
-          browser.setValue('input[id=lastName]', 'da silva')
-          browser.setValue('textarea[id=biography]', 'hhahahahahah')
-          .click("button[type=submit]")
-          .useXpath()
-          .waitForElementVisible("//a[text()='risos']", 10000)
-          .end()
-      }
+    'validates invalid emails': function (browser) {
+      const devServer = browser.globals.devServerURL
+  
+      browser
+        .url(devServer)
+        .useXpath()
+        .waitForElementVisible("//a[text()='Register']", 5000)
+        .click("//a[text()='Register']")
+        .useCss()
+        .assert.visible("button[type=submit]")
+        browser.setValue('input[id=email]', 'ayrofllmao')
+        .click("button[type=submit]")
+        .useXpath()
+        .assert.visible("//span[text()='A valid email is required']")
+        .end()
+    },
+
+    'validates small passwords': function (browser) {
+      const devServer = browser.globals.devServerURL
+  
+      browser
+        .url(devServer)
+        .useXpath()
+        .waitForElementVisible("//a[text()='Register']", 5000)
+        .click("//a[text()='Register']")
+        .useCss()
+        .assert.visible("button[type=submit]")
+        browser.setValue('input[id=password]', '12345')
+        .click("button[type=submit]")
+        .useXpath()
+        .assert.visible("//span[text()='A valid password with 6 digits or more is required']")
+        .end()
+    },
+
+    'inserts a user': function (browser) {
+      const devServer = browser.globals.devServerURL
+    
+      browser
+        .url(devServer)
+        .useXpath()
+        .waitForElementVisible("//a[text()='Register']", 5000)
+        .click("//a[text()='Register']")
+        .useCss()
+        .assert.visible("button[type=submit]")
+        browser.setValue('input[id=email]', 'ayrofllmao@gmail.com')
+        browser.setValue('input[id=password]', '123456')
+        browser.setValue('input[id=username]', 'risos')
+        browser.setValue('input[id=firstName]', 'risonho')
+        browser.setValue('input[id=lastName]', 'da silva')
+        browser.setValue('textarea[id=biography]', 'hhahahahahah')
+        .click("button[type=submit]")
+        .useXpath()
+        .waitForElementVisible("//a[text()='risos']", 10000)
+        .end()
+    },
+
   }
   
