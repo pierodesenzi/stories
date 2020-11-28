a34f554a3a8787bda2ed162d15569a62c2a0b0ef
Luis <lhzimmermann@gmail.com>
2020-11-28 11:33:07 -0300

    Setting up mock data for firebase emulator

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -9,6 +9,17 @@ var db = firebaseApp.firestore()
 
 if (process.env.NODE_ENV == "development" || "testing") {
     firebase.auth().useEmulator('http://localhost:9099/');
+    firebase.auth().createUserWithEmailAndPassword ('rivelino@gmail.com', '123456')
+    .then(user => {
+        user.user.updateProfile({
+          displayName: 'Rivelino10',
+        })
+        .then(sucess => {
+          console.log('Mock user created')
+          firebase.auth().signOut()
+        })
+    })
+    .catch(error => {})
     db.useEmulator("localhost", 4040);
 }
 
